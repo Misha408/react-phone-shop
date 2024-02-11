636f963e4a0478e635fd3579f4301ed9aebf98c3
Misha Siv <0Sivcov0@gmail.com>
2024-02-11 21:32:11 +0200

    add site

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React from 'react';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+import { Outlet } from 'react-router-dom';
+import { NavBar } from './component/NavBar';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        TodoList
-      </Provider>
+    <div data-cy="app">
+      <NavBar />
+
+      <main className="section">
+        <Outlet />
+      </main>
     </div>
   );
 };

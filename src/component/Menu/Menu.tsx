c8966551366ab04ebbac8f1f6d65425cce5501c5
Misha Siv <0Sivcov0@gmail.com>
2024-02-24 11:50:23 +0200

    fixex

--- src/component/Menu/Menu.tsx
+++ src/component/Menu/Menu.tsx
@@ -21,71 +21,74 @@ export const Menu: React.FC<Props> = ({ setMenuActive }) => {
 
   return (
     <div className="menu">
-      <nav className="menu__nav">
-        {/* eslint-disable-next-line */}
-        <NavLink
-          to="/"
-          onClick={() => setMenuActive(false)}
-        >
+      <div className="menu__content">
+        <nav className="menu__nav">
+          {/* eslint-disable-next-line */}
+          <NavLink
+            to="/"
+            onClick={() => setMenuActive(false)}
+          >
+            <img
+              className="menu__logo"
+              src="./icon/Logo.svg"
+              alt="Logo"
+            />
+          </NavLink>
+          {/* eslint-disable-next-line */}
           <img
-            className="menu__logo"
-            src="./icon/Logo.svg"
-            alt="Logo"
+            className="menu__icon-close"
+            src="./icon/Close.svg"
+            alt="Menu"
+            onClick={() => setMenuActive(false)}
           />
-        </NavLink>
-        {/* eslint-disable-next-line */}
-        <img
-          className="menu__icon menu__icon-close"
-          src="./icon/Close.svg"
-          alt="Menu"
-          onClick={() => setMenuActive(false)}
-        />
-      </nav>
-      <div className="container">
-        <ul className="menu__list">
-          <li className="menu__item">
-            <NavLink
-              to="/"
-              className={`menu__link ${getLinckClass}`}
-              onClick={() => setMenuActive(false)}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className="menu__item">
-            <NavLink
-              to="/phones"
-              className={`menu__link ${getLinckClass}`}
-              onClick={() => setMenuActive(false)}
-            >
-              Phones
-            </NavLink>
-          </li>
-          <li className="menu__item">
-            <NavLink
-              to="/tablets"
-              className={`menu__link ${getLinckClass}`}
-              onClick={() => setMenuActive(false)}
-            >
-              Tables
-            </NavLink>
-          </li>
-          <li className="menu__item">
-            <NavLink
-              to="/accessories"
-              className={`menu__link ${getLinckClass}`}
-              onClick={() => setMenuActive(false)}
-            >
-              Accessories
-            </NavLink>
-          </li>
-        </ul>
+        </nav>
+        <div className="container">
+          <ul className="menu__list">
+            <li className="menu__item">
+              <NavLink
+                to="/"
+                className={`menu__link ${getLinckClass}`}
+                onClick={() => setMenuActive(false)}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="menu__item">
+              <NavLink
+                to="/phones"
+                className={`menu__link ${getLinckClass}`}
+                onClick={() => setMenuActive(false)}
+              >
+                Phones
+              </NavLink>
+            </li>
+            <li className="menu__item">
+              <NavLink
+                to="/tablets"
+                className={`menu__link ${getLinckClass}`}
+                onClick={() => setMenuActive(false)}
+              >
+                Tables
+              </NavLink>
+            </li>
+            <li className="menu__item">
+              <NavLink
+                to="/accessories"
+                className={`menu__link ${getLinckClass}`}
+                onClick={() => setMenuActive(false)}
+              >
+                Accessories
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
 
       <div className="menu__wrap">
         <NavLink
           to="/favourites"
           className={`menu__icon ${getLinckClass}`}
+          onClick={() => setMenuActive(false)}
         >
           <div className="menu__icon__wrap">
             {!!favourites.length && (
@@ -97,7 +100,6 @@ export const Menu: React.FC<Props> = ({ setMenuActive }) => {
             <img
               src="./icon/Heart.svg"
               alt="Heart"
-              onClick={() => setMenuActive(false)}
             />
           </div>
         </NavLink>
@@ -105,6 +107,7 @@ export const Menu: React.FC<Props> = ({ setMenuActive }) => {
         <NavLink
           to="/card"
           className={`menu__icon ${getLinckClass}`}
+          onClick={() => setMenuActive(false)}
         >
           <div className="menu__icon__wrap">
             {!!cartItems.length && (
@@ -116,11 +119,9 @@ export const Menu: React.FC<Props> = ({ setMenuActive }) => {
             <img
               src="./icon/basket.svg"
               alt="Basket"
-              onClick={() => setMenuActive(false)}
             />
           </div>
         </NavLink>
-
       </div>
     </div>
   );
